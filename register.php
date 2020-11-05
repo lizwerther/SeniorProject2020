25c4a849ebe967feaee7dbc0d35a1ffc4abcccd0
maddievanbrunt <57709798+maddievanbrunt@users.noreply.github.com>
2020-11-05 14:41:45 -0500

    updating register.php

--- register.php
+++ register.php
@@ -52,13 +52,17 @@
     <form method="post" class="form-signin">
   <img class="mb-4" src="./ListifyLogo.png" alt="" width="72" height="72">
   <h1 class="h3 mb-3 font-weight-normal">Register Here</h1>
-  <label for="inputName" class="sr-only">Name</label><br>
-  <input type="name" class="form-control" placeholder="Name" name="name"  required><br>
+  <label for="inputName" class="sr-only">First Name</label><br>
+  <input type="fname" class="form-control" placeholder="First Name" name="fname"  required><br>
+  <label for="inputName" class="sr-only">Last Name</label><br>
+  <input type="lname" class="form-control" placeholder="Last Name" name="lname"  required><br>
+  <label for="inputUsername" class="sr-only">Email</label><br>
+  <input type="email"  class="form-control" placeholder="Email address" name="email" required><br>
+  <label for="inputName" class="sr-only">Phone Number</label><br>
+  <input type="number" class="form-control" placeholder="Phone Number" name="phonenumber"  required><br>
   <label for="inputName" class="sr-only">Username</label><br>
   <input type="username" class="form-control" placeholder="Username" name="username"  required><br>
-  <label for="inputUsername" class="sr-only">Email</label><br>
-  <input type="email"  class="form-control" placeholder="Email address" name="email" required>
-  <br><label for="inputPassword" class="sr-only">Password</label>
+  <label for="inputPassword" class="sr-only">Password</label>
   <br><input type="password" class="form-control" placeholder="Password" name= "password" autocomplete= "on" required>
       <br>
   <button type="submit" class="btn btn-lg btn-primary btn-block" name="submit">Submit</button>
@@ -72,20 +76,47 @@
 </html>
 
 <?php
+$UserID = 0; 
 //$conn = mysqli_connect('localhost', 'listify', '', 'general');
+$conn = oci_connect('asheerin', 'sP01397995', 'csdb2.csc.Villanova.edu:1521/orcl.villanova.edu');
 require "conn.php"; 
 if(!$conn){ 
-   // echo "connection fail";
+   echo "connection fail";
 }
-//else {echo "connection success";}
+// else {echo "connection success";}
+
 if(isset($_POST["submit"])){ 
-    $name = $_POST["name"];
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $email = $_POST["email"];
+    $phonenumber = $_POST["phonenumber"]; 
     $username = $_POST["username"]; 
-    $email = $_POST["email"]; 
     $password = $_POST["password"];
-    $query = "INSERT INTO login (name,username,email,password) VALUES ('$name', '$username', '$email', '$password')"; 
+
+    $query = ("SELECT PROFILEID FROM PROFILES WHERE rownum = 1 ORDER BY PROFILEID DESC"); 
+    $s = oci_parse($conn, $query);
+    oci_execute($s);
+    $row = oci_fetch_array($s, OCI_NUM);
+    $UserID= $row[0];
+
+    $UserID = $UserID + 1; 
+  
+    
+    $sql = "INSERT INTO PROFILES (PROFILEID, fname, lname, email, phonenumber, username, pword) VALUES ('$UserID', '$fname', '$lname', '$email', '$phonenumber', '$username', '$password')";
+
+    $stmt = oci_parse($conn, $sql);
+    $res = oci_execute($stmt);
+    if( !$res ){
+        $error = oci_error($stmt);
+        echo "Error: " . $error['message'] . "\n";
+    }else{
+      //  echo "OK\n";
+    }
+
+
+    //$query = "INSERT INTO PROFILES (PROFILEID, fname, email, pword) VALUES ('$username', '$name', '$email', '$password')"; 
     //echo $query;
-    $result = mysqli_query($conn,$query); 
+   // $result = mysqli_query($conn,$query); 
     //echo $result; 
    
    // if($result){ 
