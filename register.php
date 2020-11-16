3344061205b383ed16c20b47d907a22608fcd25c
maddievanbrunt <57709798+maddievanbrunt@users.noreply.github.com>
2020-11-15 19:09:37 -0500

    update to register.php with interests included

--- register.php
+++ register.php
@@ -32,7 +32,6 @@
   </head>
   <body class="text-center">
     <form method="post" class="form-signin">
-  <div class="main">
   <img class="mb-4" src="./ListifyLogo.png" alt="" width="72" height="72">
   <h1 class="h3 mb-3 font-weight-normal">Register Here</h1>
   <label for="inputName" class="sr-only">First Name</label><br>
@@ -49,53 +48,96 @@
   <br><input type="password" class="form-control" placeholder="Password" name= "password" autocomplete= "on" required><br>
   <label for="inputBio" class="sr-only">Bio</label>
   <br><input type="bio" class="form-control" placeholder="Tell Us About Yourself!" name= "bio" required>
-  <!-- <br> -->
+  <!--<input name="interests" for ="inputInterests" > -->
+  <p>Please select all of your interests!</p>
+    Movies: <input type="checkbox" name="interests[]" value="Movies"  /><br />
+    TV Shows: <input type="checkbox" name="interests[]" value="Tv Shows"  /><br /> 
+    Podcasts: <input type="checkbox" name="interests[]" value="Podcasts"  /><br /> 
+    Books: <input type="checkbox" name="interests[]" value="Books"  /><br />
+    Food: <input type="checkbox" name="interests[]" value="Food"  /><br /> 
+    Fashion: <input type="checkbox" name="interests[]" value="Fashion"  /><br /> 
+    Games: <input type="checkbox" name="interests[]" value="Games"  /><br /> 
+<br>
   <button type="submit" class="btn btn-lg btn-primary btn-block" name="submit">Submit</button>
   <!--<p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p> -->
   <div class="links">
   <a href="index.php">Already Have an Account? Log in Here</a>
   </div>
-</div>
-
 </form>
 </body>
 </html>
 
 <?php
-$UserID = 0; 
-$conn = mysqli_connect('localhost', 'listify', '', 'general');
+//$UserID = 0; 
+//$conn = mysqli_connect('localhost', 'listify', '', 'general');
 $conn = oci_connect('asheerin', 'sP01397995', 'csdb2.csc.Villanova.edu:1521/orcl.villanova.edu');
 require "conn.php"; 
 if(!$conn){ 
    echo "connection fail";
 }
-else {echo "connection success";}
+// else {echo "connection success";}
 
 if(isset($_POST["submit"])){ 
     $fname = $_POST["fname"];
     $lname = $_POST["lname"];
     $email = $_POST["email"];
-  //  $phonenumber = $_POST["phonenumber"]; 
     $username = $_POST["username"]; 
     $password = $_POST["password"];
     $bio = $_POST["bio"];
+   if(in_array('movies', $_POST['interests'])){
+        $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'movies')";
+        $stmt = oci_parse($conn, $sql);
+        $res = oci_execute($stmt);
+    }
+    if(in_array('tvshows', $_POST['interests'])){
+      $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'tvshows')";
+      $stmt = oci_parse($conn, $sql);
+      $res = oci_execute($stmt);
+  }
+  if(in_array('podcasts', $_POST['interests'])){
+    $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'podcasts')";
+    $stmt = oci_parse($conn, $sql);
+    $res = oci_execute($stmt);
+  }
+  if(in_array('books', $_POST['interests'])){
+  $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'books')";
+  $stmt = oci_parse($conn, $sql);
+  $res = oci_execute($stmt);
+  }
+  if(in_array('food', $_POST['interests'])){
+    $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'food')";
+    $stmt = oci_parse($conn, $sql);
+    $res = oci_execute($stmt);
+}
+if(in_array('fashion', $_POST['interests'])){
+  $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'fashion')";
+  $stmt = oci_parse($conn, $sql);
+  $res = oci_execute($stmt);
+}
+if(in_array('games', $_POST['interests'])){
+  $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'games')";
+  $stmt = oci_parse($conn, $sql);
+  $res = oci_execute($stmt);
+}
 
-   // $query = ("SELECT PROFILEID FROM PROFILES WHERE rownum = 1 ORDER BY PROFILEID DESC"); 
-   // $s = oci_parse($conn, $query);
-   // oci_execute($s);
-   // $row = oci_fetch_array($s, OCI_NUM);
-   // $UserID= $row[0];
 
-   // $UserID = $UserID + 1; 
+   //$query = ("SELECT PROFILEID FROM PROFILES WHERE rownum = 1 ORDER BY PROFILEID DESC"); 
+   //$s = oci_parse($conn, $query);
+   //oci_execute($s);
+   //$row = oci_fetch_array($s, OCI_NUM);
+   //$UserID= $row[0];
+
+   //$UserID = $UserID + 1; 
   
-    
+    //need to add that the username already exists
     $sql = "INSERT INTO PROFILES (fname, lname, email, username, pword, bio) VALUES ('$fname', '$lname', '$email', '$username', '$password', '$bio')";
 
     $stmt = oci_parse($conn, $sql);
     $res = oci_execute($stmt);
     if( !$res ){
         $error = oci_error($stmt);
-        echo "Error: " . $error['message'] . "\n";
+        echo "An account with that password already exists!";
+        //echo "Error: " . $error['message'] . "\n";
     }else{
       //  echo "OK\n";
       header("Location: index.php");
