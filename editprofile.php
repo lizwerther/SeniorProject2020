ca3e66e376d01aeb5525da9b145ad312e99533e8
maddievanbrunt <57709798+maddievanbrunt@users.noreply.github.com>
2020-11-16 19:52:29 -0500

    register and change profile done checkkkk

--- editprofile.php
+++ editprofile.php
@@ -53,7 +53,7 @@ if(!$conn){
 
   <br>
   <label for="inputBio" class="sr-only">Edit Bio</label>
-  <br><input type="bio" class="form-control" placeholder="New Bio" name= "bio" ><br>
+  <br><input type="EditBio" class="form-control" placeholder="New Bio" name= "bio_new" ><br>
   <button type="submit" class="btn btn-lg btn-primary btn-block" name="EditBio">Edit</button>
 
   </div>
@@ -61,13 +61,13 @@ if(!$conn){
 
   <p>Please select all of your new interests!</p>
     <div class="interests">
-    Movies: <input type="checkbox" name="interests[]" value="Movies"  /><br />
-    TV Shows: <input type="checkbox" name="interests[]" value="Tv Shows"  /><br /> 
-    Podcasts: <input type="checkbox" name="interests[]" value="Podcasts"  /><br /> 
-    Books: <input type="checkbox" name="interests[]" value="Books"  /><br />
-    Food: <input type="checkbox" name="interests[]" value="Food"  /><br /> 
-    Fashion: <input type="checkbox" name="interests[]" value="Fashion"  /><br /> 
-    Games: <input type="checkbox" name="interests[]" value="Games"  /><br /> 
+    Movies: <input type="checkbox" name="interests[]" value="movies"  /><br />
+    TV Shows: <input type="checkbox" name="interests[]" value="tvshows"  /><br /> 
+    Podcasts: <input type="checkbox" name="interests[]" value="podcasts"  /><br /> 
+    Books: <input type="checkbox" name="interests[]" value="books"  /><br />
+    Food: <input type="checkbox" name="interests[]" value="food"  /><br /> 
+    Fashion: <input type="checkbox" name="interests[]" value="fashion"  /><br /> 
+    Games: <input type="checkbox" name="interests[]" value="games"  /><br /> 
     </div>
   <button type="submit" class="btn btn-lg btn-primary btn-block" name="EditInterests">Edit</button>
   <!--<p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p> -->
@@ -94,47 +94,60 @@ if(isset($_POST["EditUsername"])){
       $sql = "UPDATE PROFILES SET USERNAME = '$username_new' WHERE USERNAME= '$username_old'";
       $stmt2 = oci_parse($conn, $sql);
       $res2 = oci_execute($stmt2);
+      session_destroy(); 
+      header("Location:index.php?msgunchange");
   }
-  session_destroy(); 
-  header("Location:index.php?msgunchange");
 }
 
-// $bio = $_POST["bio"];
-//    if(in_array('movies', $_POST['interests'])){
-//         $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'movies')";
-//         $stmt = oci_parse($conn, $sql);
-//         $res = oci_execute($stmt);
-//     }
-//     if(in_array('tvshows', $_POST['interests'])){
-//       $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'tvshows')";
-//       $stmt = oci_parse($conn, $sql);
-//       $res = oci_execute($stmt);
-//   }
-//   if(in_array('podcasts', $_POST['interests'])){
-//     $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'podcasts')";
-//     $stmt = oci_parse($conn, $sql);
-//     $res = oci_execute($stmt);
-//   }
-//   if(in_array('books', $_POST['interests'])){
-//   $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'books')";
-//   $stmt = oci_parse($conn, $sql);
-//   $res = oci_execute($stmt);
-//   }
-//   if(in_array('food', $_POST['interests'])){
-//     $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'food')";
-//     $stmt = oci_parse($conn, $sql);
-//     $res = oci_execute($stmt);
-// }
-// if(in_array('fashion', $_POST['interests'])){
-//   $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'fashion')";
-//   $stmt = oci_parse($conn, $sql);
-//   $res = oci_execute($stmt);
-// }
-// if(in_array('games', $_POST['interests'])){
-//   $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username', 'games')";
-//   $stmt = oci_parse($conn, $sql);
-//   $res = oci_execute($stmt);
-// }
+if(isset($_POST["EditBio"])){ 
+    $bio_new = $_POST["bio_new"]; 
+    $sql = "UPDATE PROFILES SET bio = '$bio_new' WHERE USERNAME= '$username_old'";
+    $stmt2 = oci_parse($conn, $sql);
+    $res2 = oci_execute($stmt2);
+}
+if(isset($_POST["EditInterests"])){ 
+    $sql = "DELETE FROM INTERESTS WHERE USERNAME= '$username_old'";
+    $stmt = oci_parse($conn, $sql);
+    $res = oci_execute($stmt);
+
+    if(in_array('movies', $_POST['interests'])){
+        $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username_old', 'movies')";
+        $stmt = oci_parse($conn, $sql);
+        $res = oci_execute($stmt);
+    }
+    if(in_array('tvshows', $_POST['interests'])){
+      $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username_old', 'tvshows')";
+      $stmt = oci_parse($conn, $sql);
+      $res = oci_execute($stmt);
+  }
+    if(in_array('podcasts', $_POST['interests'])){
+    $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username_old', 'podcasts')";
+    $stmt = oci_parse($conn, $sql);
+    $res = oci_execute($stmt);
+  }
+    if(in_array('books', $_POST['interests'])){
+    $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username_old', 'books')";
+    $stmt = oci_parse($conn, $sql);
+    $res = oci_execute($stmt);
+  }
+    if(in_array('food', $_POST['interests'])){
+    $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username_old', 'food')";
+    $stmt = oci_parse($conn, $sql);
+    $res = oci_execute($stmt);
+}
+    if(in_array('fashion', $_POST['interests'])){
+    $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username_old', 'fashion')";
+    $stmt = oci_parse($conn, $sql);
+    $res = oci_execute($stmt);
+}
+    if(in_array('games', $_POST['interests'])){
+    $sql = "INSERT INTO INTERESTS (username, interest) VALUES ('$username_old', 'games')";
+    $stmt = oci_parse($conn, $sql);
+    $res = oci_execute($stmt);
+}
+
+}
+
 
 
 ?>
